5ce17346b3720297ba3634b70b599068a7315c0d
Madireddi Lokesh Babu - Munich-MR - external <ny49779@munich.munichre.com>
2025-02-10 18:31:36 +0530

    commit

--- python.py
+++ python.py
@@ -1,1 +1,24 @@
-print("Hello World")
+import os
+import datetime
+
+# Define the fireworks pattern
+fireworks = [
+    "   X   ",  
+    "  X X  ",  
+    " X   X ",  
+    "X  X  X",  
+    " X   X ",  
+    "  X X  ",  
+    "   X   "
+]
+
+# Set the starting date for commits
+start_date = datetime.date(2024, 1, 1)
+
+# Loop through the pattern and create commits
+for i, row in enumerate(fireworks):
+    for j, cell in enumerate(row):
+        if cell == 'X':  # If this position should have a commit
+            commit_date = start_date + datetime.timedelta(days=j + (i * 7))  
+            os.system(f'GIT_COMMITTER_DATE="{commit_date}" git commit --allow-empty -m "Fireworks commit" --date="{commit_date}"')
+            os.system('git push')  # Push commit to GitHub
